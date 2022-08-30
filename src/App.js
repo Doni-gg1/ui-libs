5a8580d6bad1c04dfbcb8c563e315960925b9ba7
Daniel <donisuper04@gmail.com>
2022-08-30 20:43:42 +0600

    add all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { Accordion, AccordionSummary, Typography, AccordionDetails } from '@mui/material';
+import { Form, Input, Button } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
+
+
+  const validateMessages = {
+    types: {
+      email: 'Email is not valid'
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+      <Accordion>
+        <AccordionSummary>
+          <Typography sx={{ color: 'green' }} align='center' variant='h3'>About web develop</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Front-end</Typography>
+          <Typography>Back-end</Typography>
+          <Typography>Data Base</Typography>
+        </AccordionDetails>
+      </Accordion>
+
+
+      <Form validateMessages={validateMessages} onFinish={() => console.log('finished')}>
+        <Form.Item
+          label='Username'
+          name='userName'
+          rules={[{ required: true, message: 'This field required' }]}
         >
-          Learn React
-        </a>
-      </header>
+          <Input />
+        </Form.Item>
+        <Form.Item 
+          label='Email'
+          name='email'
+          rules={[{ required: true, message: 'You need input your email'}, { type: 'email', message: 'Email is not valid' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
+      
     </div>
   );
 }
